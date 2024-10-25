4baadaac18ec8647cc2d45282d69e8585dc72894
zibin <chenzibin2017@outlook.com>
2024-10-24 20:32:18 -0400

    look check.

--- test_project2.py
+++ test_project2.py
@@ -28,3 +28,19 @@ if __name__ == '__main__':
     print("--------- Task6 find path in LowestMaxWeightFirst algorithm ---------")
     for i in range(4):
         print(routers[i], routers[i + 1], 'Path:', net.findPathMaxWeight(routers[i], routers[i + 1]))
+
+    print("--------- Task7 identify loop ---------")
+
+    route = {
+        "MelbourneAustralia3870": "MelbourneAustralia3882",
+        "MelbourneAustralia3882": "MelbourneAustralia734",
+        "MelbourneAustralia734": "MelbourneAustralia3881",
+        "MelbourneAustralia3881": "MelbourneAustralia3870"
+    }  # This one is a loop
+    print(net.checkLoop(route))
+
+    route = {
+        "RockhamptonAustralia4195": "BrisbaneAustralia1769",
+        "BrisbaneAustralia1769": "TownsvilleAustralia4282"
+    } # This one is NOT a loop
+    print(net.checkLoop(route))
